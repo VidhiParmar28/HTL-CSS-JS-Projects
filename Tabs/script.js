3b9f60006d377435eca119b96cc91b9706578d0a
Vidhi M Parmar <vidhi28092002@gmail.com>
2026-07-14 09:00:30 -0700

    Add files via upload

---
+++ Tabs/script.js
@@ -0,0 +1,20 @@
+// var btns = document.getElementsByClassName("tablinks");
+
+// for(var i=0;i<btns.length ;i++){
+//     btns[i].addEventListener("click", openCity()){
+
+//         this.classList.toggle("active");
+
+//         var content = document.getElementsByClassName("tabcontent");
+// for(var j=0;j<content.length;j++){
+//    if( content[j].style.display === "block"){
+//      content[j].style.display = "none";
+//    }
+//    else{
+//     content[j].style.display = "block";
+//    }
+// }
+
+//         // var content = this.
+//     })
+// }
