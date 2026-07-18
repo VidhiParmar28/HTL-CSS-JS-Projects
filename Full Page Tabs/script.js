c9a9e9243792edfbb0dd2191a593ce23f19f9b0e
Vidhi M Parmar <vidhi28092002@gmail.com>
2026-07-18 12:51:45 -0700

    Add files via upload

---
+++ Full Page Tabs/script.js
@@ -0,0 +1,39 @@
+let tablinks = document.querySelectorAll(".tablink");
+let tabcontents = document.querySelectorAll(".tabcontent");
+let arr = ["#F44336", "#04AA6D", "#2196F3", "#FF5722"];
+
+tabcontents.forEach((content)=>{
+    content.style.display = "none";
+})
+
+
+tabcontents[0].style.display = "block";
+tabcontents[0].style.backgroundColor = arr[0];
+tablinks[0].classList.add("active");
+
+
+
+tablinks.forEach((link)=>{
+    link.classList.remove("active");
+})
+
+tablinks.forEach((link,index)=>{
+    link.addEventListener("click",() =>{
+        tabcontents.forEach((content)=>{
+            content.style.display = "none";
+        });
+
+        tablinks.forEach((l)=>{
+            l.classList.remove("active");
+        });
+
+        tabcontents[index].style.display = "block";
+        tabcontents[index].style.backgroundColor = arr[index];
+        link.classList.add("active");
+
+
+    });
+});
+
+
+
