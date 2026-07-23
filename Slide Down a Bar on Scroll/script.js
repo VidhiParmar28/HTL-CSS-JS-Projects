7a0b125e8285eba4afcbfd7ff90dcb9fe32a37ef
Vidhi M Parmar <vidhi28092002@gmail.com>
2026-07-23 11:21:12 -0700

    Add files via upload

---
+++ Slide Down a Bar on Scroll/script.js
@@ -0,0 +1,14 @@
+let navbar = document.getElementById("navbar");
+
+window.onscroll = function(){
+    scrollFunction();
+};
+
+function scrollFunction(){
+    if(document.body.scrollTop > 20 || document.documentElement.scrollTop >20) {
+        navbar.style.top = "0";
+    }
+    else{
+        navbar.style.top = "-100px";
+    }
+}
